46d39faeb23a4a70d2051357eedee2aa51be5281
neksen <s.kravtsov@hotmail.com>
2024-09-27 22:13:38 +0300

    add page link in admin edit page (pages)

--- lms/pages/models.py
+++ lms/pages/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 from users.models import User
 
@@ -22,3 +23,6 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('pages:page', kwargs={'slug': self.slug})
